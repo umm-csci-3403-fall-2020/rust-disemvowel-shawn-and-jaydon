2003ca1b89b0e78d8c36da03daa4dfb5ed930b30
Utkarsh Kumar <46744598+UtkarshKr007@users.noreply.github.com>
2020-11-29 15:01:09 -0600

    Update tests to match package name

--- src/main.rs
+++ src/main.rs
@@ -116,7 +116,7 @@ mod tests {
         use super::*;
         #[test]
         fn requires_two_arguments() {
-            let mut cmd = Command::cargo_bin("rust-disemvowel").unwrap();
+            let mut cmd = Command::cargo_bin("disemvowel-in-rust").unwrap();
             cmd.arg("1");
             cmd.assert()
                 .failure()
@@ -124,7 +124,7 @@ mod tests {
         }
         #[test]
         fn requires_read_file() {
-            let mut cmd = Command::cargo_bin("rust-disemvowel").unwrap();
+            let mut cmd = Command::cargo_bin("disemvowel-in-rust").unwrap();
             cmd.arg("/this/path/does/not/exist")
                 .arg("output/path/doesnt/matter");
             cmd.assert()
